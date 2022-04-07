8873febbf52a6fe2430eaf3a2571a71650853e6e
minertjw <thomas@wood-lands.com>
2022-04-07 22:02:08 +1000

    Major Update: All hardware functioning. Attempts made at encoder based line straight line travel. New functions added.

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ from motor import Motor
 from machine import Pin, PWM
 from machine import time_pulse_us
 from encoder import Encoder
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+
 
 # Initialisation Statements:
 
-# Line Sensor
-line_sensorL = Pin(28, Pin.IN)
+# Line Sensors
 line_sensorM = Pin(26, Pin.IN)
-line_sensorR = Pin(27, Pin.IN)
+line_sensorL = Pin(27, Pin.IN)
+line_sensorR = Pin(28, Pin.IN)
 
 # Motors
 motor_left = Motor("left", 8, 9, 6)
 motor_right = Motor("right", 10, 11, 7)
 
-# Ultrasonic Sensor
-ultrasonic_sensor = sonic(3, 2)
+# Ultrasonic Distance Sensor
+TRIG = 3
+ECHO = 2
+ultrasonic_sensor = sonic(TRIG, ECHO)
 
 # Servo
 pwm = PWM(Pin(15))
@@ -28,18 +33,34 @@ ENC_L = 18  # pin2y34ry8
 ENC_R = 19
 enc = Encoder(ENC_L, ENC_R)
 
+# OLED Screen
+i2c = I2C(0, sda=Pin(12), scl=Pin(13))
+oled = SSD1306_I2C(128, 64, i2c)
 
-def setServoAngle(angle=90):  # Changes the angle the front mounted servo is pointing
+
+def setServoAngle(angle=100):  # Changes the angle the front mounted servo is pointing
     position = int(8000 * (angle / 180) + 1000)  # Convert angle into [1000, 9000]
     # ...range
     pwm.duty_u16(position)  # set duty cycle #
 
 
+def lookForward():
+    setServoAngle(100)
+
+
+def lookLeft():
+    setServoAngle(167)
+
+
+def lookRight():
+    setServoAngle(25)
+
+
 def forward(speed=50):  # Moves the robot forward at a selected speed
     motor_left.set_forwards()
     motor_right.set_forwards()
-    motor_left.duty(speed)
-    motor_right.duty(speed)
+    motor_left.duty(int(speed))
+    motor_right.duty(int(speed))
 
 
 def backward(speed=50):  # Moves the robot backward at a selected speed
@@ -84,52 +105,84 @@ def stopDist(dist=60, reason="nothing"):  # Stops the robot a specified distance
 
 
 def distCheck():  # Checks the distance between the ultrasonic sensor and what it is pointing at
-    return ultrasonic_sensor.distance_mm()
+    measurement = ultrasonic_sensor.distance_mm()
+    return measurement
 
 
 def floorCheck():  # Checks the colour of the floor
     return line_sensorM.value()
 
 
-def spin(angle=120, direction="clock", speed=40):  # Spins the robot, can select angle, direction and speed
+def spin(angle=120, direction="clock"):  # Spins the robot, can select angle, direction and speed
     if direction == "clock":
         motor_left.set_forwards()
         motor_right.set_backwards()
     else:
         motor_left.set_backwards()
         motor_right.set_forwards()
-    motor_right.duty(speed)
-    motor_left.duty(speed)
+    motor_right.duty(50)
+    motor_left.duty(50)
     time.sleep(angle / 100)
     stop()
 
-while True:
-    print(line_sensorM.value())
-    time.sleep(0.5)
-    print(line_sensorR.value())
-    time.sleep(0.5)
-    print(line_sensorL.value())
-    time.sleep(0.5)
-    print("")
-    time.sleep(0.5)
-    time.sleep(0.5)
 
-'''# Competency Task Operational Code:
-while True:
-    forward(50)
-    stopDist(60, "floor")
-    if floorCheck() != 1:
-        backward(50)
-        time.sleep(1)
-        spin(90, "clock")
-    else:
-        break'''
+def tickRight():
+    return enc.get_right()
 
-'''# Sweep servo between 0 and 180 degrees, in increments of 1 degree
+
+def tickLeft():
+    return enc.get_left()
+
+
+def encOnScreen():
+    oled.text(str(enc.get_left()), 0, 0)
+    oled.text(str(enc.get_right()), 32, 0)
+    oled.show()
+    oled.fill(0)
+
+# Operational Code:
+
+'''setServoAngle()
+oled.text("A long time ago", 0, 16)
+oled.text(" in a galaxy far ", 0, 24)
+oled.text("far away...", 0, 32)
+oled.show()
+time.sleep(5)
+oled.fill(0)
+oled.show()
+time.sleep(3)
+for i in range(64,-10,-1):
+    oled.text("STAR WARS", 25, i)
+    oled.show()
+    time.sleep(0.03)
+    oled.fill(0)'''
+setServoAngle()
+
+
+'''forward()
 while True:
-    for pos in range(0, 180, 1):
-        setServoAngle(pos)  # Set servo to desired angle
-        time.sleep(0.01)  # Wait 10 ms to reach angle
-    for pos in range(180, 0, -1):
-        setServoAngle(pos)  # Set servo to desired angle
-        time.sleep(0.01)  # Wait 10 ms to reach angle'''
+    while tickRight() > tickLeft():
+        motor_right.duty(int(50*(tickLeft()/tickRight())))
+        encOnScreen()
+    forward()
+    while tickLeft() > tickRight():
+        motor_left.duty(int(50*0.8))
+        encOnScreen()
+    forward()
+    encOnScreen()'''
+
+
+'''oled.text("A long time ago", 0, 16)
+oled.text(" in a galaxy far ", 0, 24)
+oled.text("far away...", 0, 32)
+oled.show()
+time.sleep(5)
+oled.fill(0)
+oled.show()
+time.sleep(3)
+for i in range(64,-10,-1):
+    oled.text("STAR WARS", 25, i)
+    oled.show()
+    time.sleep(0.03)
+    oled.fill(0)'''
+
