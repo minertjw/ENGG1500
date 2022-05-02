60590cc0c6379b1276b6fa56264d109961016b5b
minertjw <thomas@wood-lands.com>
2022-05-02 22:54:01 +1000

    Moved all commented code into callable functions Rewrote roundabout code using new line follow function Now always turns left and always takes the first exit

--- main.py
+++ main.py
@@ -34,9 +34,8 @@ enc = Encoder(ENC_L, ENC_R)
 i2c = I2C(0, sda=Pin(12), scl=Pin(13))
 oled = SSD1306_I2C(128, 64, i2c)
 
-
-def setServoAngle(
-        angle=100):  # Changes the angle the front mounted servo is pointing. Angle is inverted, (Left is 167, right is 25)
+# Servo Controls
+def setServoAngle(angle=100):  # Changes the angle the front mounted servo is pointing. Angle is inverted, (Left is 167, right is 25)
     position = int(8000 * (angle / 180) + 1000)  # Convert angle into [1000, 9000]
     pwm.duty_u16(position)  # set duty cycle #
 def lookForward():
@@ -46,7 +45,7 @@ def lookLeft():
 def lookRight():
     setServoAngle(25)
 
-
+# Main Movement Controls
 def forward(speed=50):  # Moves the robot forward at a selected speed
     motor_left.set_forwards()
     motor_right.set_forwards()
@@ -55,19 +54,18 @@ def forward(speed=50):  # Moves the robot forward at a selected speed
 def backward(speed=50):  # Moves the robot backward at a selected speed
     motor_left.set_backwards()
     motor_right.set_backwards()
-    motor_left.duty(int(speed))
+    motor_left.duty(int(speed*0.9))
     motor_right.duty(int(speed))
 
-
-def turnLeft(speed=50,
-             intensity=25):  # Imposes a ratio on the motors, determined by the direction and intensity out of 100
+# Turning Controls (Kinda Useless Currently)
+def turnLeft(speed=50, intensity=25):  # Imposes a ratio on the motors, determined by the direction and intensity out of 100
     forward(speed)
     motor_left.duty(((100 - intensity) / 100) * speed)
 def turnRight(speed=50, intensity=25):
     forward(speed)
     motor_right.duty(((100 - intensity) / 100) * speed)
 
-
+# Stop Options
 def stop():  # Stops the robot moving
     motor_left.duty(0)
     motor_right.duty(0)
@@ -83,7 +81,7 @@ def stopDist(dist=60):  # Stops the robot a specified distance in mm from an obs
     clearScreen()
     stop()
 
-
+# Sensor Data
 def distCheck():  # Checks the distance between the ultrasonic sensor and what it is pointing at
     return ultrasonic_sensor.distance_mm()
 def floorCheck(pick="M"):  # Checks the colour of the floor
@@ -93,8 +91,13 @@ def floorCheck(pick="M"):  # Checks the colour of the floor
         return line_sensorL.value()
     else:
         return line_sensorM.value()
+def allFloorCheck():
+    if line_sensorR.value() + line_sensorL.value() + line_sensorM.value() == 3:
+        return True
+    else:
+        return False
 
-
+# Spin Controls
 def spin(angle=120, direction="clock"):  # Spins the robot, can select angle, direction and speed
     if direction == "clock":
         motor_left.set_forwards()
@@ -106,16 +109,16 @@ def spin(angle=120, direction="clock"):  # Spins the robot, can select angle, di
         motor_right.set_forwards()
         motor_right.duty(40)
         motor_left.duty(70)
-    time.sleep(angle / 100)
+    time.sleep(angle/100)
     stop()
 
-
+# Easy Encoder Gets
 def tickRight():
     return enc.get_right()
 def tickLeft():
     return enc.get_left()
 
-
+# Screen Controls
 def encOnScreen():
     oled.text(str(enc.get_left()), 10, 0)
     oled.text(str(enc.get_right()), 42, 0)
@@ -131,70 +134,75 @@ def clearScreen():
     oled.fill(0)
     oled.show()
 
+# Locked Operational Code (Can't escape, runs forever)
+def lineFollow():
+    while True:
+        forward(45)
+        while floorCheck("L") == 1:
+            spin(10, "counter")
+        while floorCheck("R") == 1:
+            spin(10, "clock")
+def steerServo():
+    while True:
+        encOnScreen()
+        setServoAngle(100+enc.get_right()-enc.get_left())
 
-# Line Follow Code:
-time.sleep(2)
+# Miscellaneous
+def starWars():
+    setServoAngle()
+    oled.text("A long time ago", 0, 16)
+    oled.text(" in a galaxy far ", 0, 24)
+    oled.text("far away...", 0, 32)
+    oled.show()
+    time.sleep(5)
+    oled.fill(0)
+    oled.show()
+    time.sleep(3)
+    for i in range(64,-10,-1):
+        oled.text("STAR WARS", 25, i)
+        oled.show()
+        time.sleep(0.03)
+        oled.fill(0)
+
+# Operational Code:
+
+lineFollow()
+
+# lineFollow is infinitely looped, roundabout code below will not run unless lineFollow() is removed:
+# Below is modified line follow code that includes a check within both while loops for the presence of a roundabout
+
+check = False
 while True:
     forward(45)
+    if check:
+        break
+
     while floorCheck("L") == 1:
         spin(10, "counter")
+        if allFloorCheck():
+            while floorCheck("R") == 1:
+                turnLeft(45, 100)
+            check = True
+            break
+
     while floorCheck("R") == 1:
         spin(10, "clock")
+        if allFloorCheck():
+            while floorCheck("R") == 1:
+                turnLeft(45, 100)
+            check = True
+            break
 
+# Now for some classic line follow code, but where it only sees using the right sensor.
+# If it sees the left edge, then it knows there is an exit, and attempts to take it.
+while True:
+    forward(45)
+    while floorCheck("R") == 1:
+        spin(10, "clock")
+    if floorCheck("L") == 1:
+        while floorCheck("M") == 1:
+            turnLeft(40,100)
+        break
 
-# Rotate Servo with ratio of wheel rotation:
-'''while True:
-    encOnScreen()
-    setServoAngle(100+enc.get_right()-enc.get_left())
-    if False:
-        break'''
-
-# Roundabout code
-'''while True:
-    forward(40)                     # this is just curved line follow
-    if floorCheck("R") == 1:        #
-        while floorCheck() == 0:    #
-            turnRight(40)           #
-        forward(40)                 #
-    if floorCheck("L") == 1:        #
-        while floorCheck() == 0:    #
-            turnLeft(40)            #
-        forward(40)                 #
-    i = 0                           # original code starts here
-    while i <= 4:                   # i is so it only runs 4 time andd the moves on
-        if floorCheck("L") == 1 and floorCheck("R") == 1:  # i want it so it only runs when both floor check L&R are =1
-            while floorCheck() == 1:
-                turnLeft(40)
-            forward(40)
-        i += 1
-        if floorCheck("L") == 1:     #this is to exit roundabout
-            while floorCheck() == 1:
-                turnLeft(40)
-            forward(40)
-    if i > 4:                           # if it loops 4 time its gonna enter the roundabout to the right
-        if floorCheck("R") == 1 and floorCheck("L") == 1:
-            while floorCheck() == 1:
-                turnRight(40)
-            forward(40)
-        i += 1
-        if floorCheck("R") == 1:
-            while floorCheck() == 1:
-                turnRight(40)
-            forward(40)'''
-
-# Star Wars on screen
-'''setServoAngle()
-oled.text("A long time ago", 0, 16)
-oled.text(" in a galaxy far ", 0, 24)
-oled.text("far away...", 0, 32)
-oled.show()
-time.sleep(5)
-oled.fill(0)
-oled.show()
-time.sleep(3)
-for i in range(64,-10,-1):
-    oled.text("STAR WARS", 25, i)
-    oled.show()
-    time.sleep(0.03)
-    oled.fill(0)'''
+lineFollow()
 
